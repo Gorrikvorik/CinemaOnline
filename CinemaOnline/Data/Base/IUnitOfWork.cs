9fe4f6645c11f81af7493d1df4508b27005e7b12
RuslanI <Ruslan>
2024-07-30 11:56:05 +0300

    реализация UnitOfWork

---
+++ CinemaOnline/Data/Base/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CinemaOnline.Data.Base
+{
+    public interface IUnitOfWork
+    {
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        void Dispose();
+        IEntityBaseRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntityBase, new();
+        Task RollBackAsync();
+    }
+}
---
+++ CinemaOnline/Data/Base/UnitOfWork.cs
@@ -0,0 +1,81 @@
+﻿using CinemaOnline.Data.DatabaseContext;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CinemaOnline.Data.Base
+{
+    public class UnitOfWork : IDisposable, IUnitOfWork
+    {
+        private readonly CinemaDBContext _context;
+        private Dictionary<Type, object>? _repos;
+        private IDbContextTransaction? _transaction;
+        private bool disposedValue;
+
+        public UnitOfWork(CinemaDBContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEntityBaseRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntityBase, new()
+        {
+            if (_repos == null)
+            {
+                _repos = new Dictionary<Type, object>();
+            }
+            var type = typeof(TEntity);
+            if (!_repos.ContainsKey(type))
+            {
+                _repos[type] = new EntityBaseRepository<TEntity>(_context);
+            }
+            return (IEntityBaseRepository<TEntity>)_repos[type];
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+
+        }
+
+        public async Task CommitAsync()
+        {
+            await _transaction.CommitAsync();
+
+        }
+
+        public async Task RollBackAsync()
+        {
+            await _transaction.RollbackAsync();
+            DisposeTransaction();
+        }
+
+        private void DisposeTransaction()
+        {
+            _transaction?.Dispose();
+            _transaction = null!;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    DisposeTransaction();
+                    _context.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+        ~UnitOfWork()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
